57d61a08aaabc6a102b90af9f1ca3150b57ce523
Matthias Fasching <Matthias.Fasching@zuehlke.com>
2019-10-31 10:52:51 +0100

    Update cask to version 978.1d

--- Casks/trader-workstation-latest.rb
+++ Casks/trader-workstation-latest.rb
@@ -1,6 +1,6 @@
 cask 'trader-workstation-latest' do
-  version :latest
-  sha256 :no_check
+  version '978,978.1d'
+  sha256 'c5d2e3cb8ea9871fa653945005aa77146205145f7f55faa0b72b52db1de977bf'
 
   url 'https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-macosx-x64.dmg'
   name 'Trader Workstation'
@@ -9,13 +9,13 @@ cask 'trader-workstation-latest' do
   conflicts_with cask: 'trader-workstation'
 
   installer script: {
-                      executable: "#{staged_path}/Trader Workstation 976 Installer.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{staged_path}/Trader Workstation #{version.before_comma} Installer.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 
   uninstall quit:   'com.install4j.5889-6375-8446-2021.22',
             script: {
-                      executable: "#{ENV['HOME']}/Applications/Trader Workstation 976/Trader Workstation 976 Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{ENV['HOME']}/Applications/Trader Workstation #{version.before_comma}/Trader Workstation #{version.before_comma} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 end
