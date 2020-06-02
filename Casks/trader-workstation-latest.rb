06c16f5a920434ab3f0a69eaf52c7fbc4fd89bad
Matthias Fasching <matthias.fasching@zuehlke.com>
2020-06-02 11:50:56 +0200

    Fix installation

--- Casks/trader-workstation-latest.rb
+++ Casks/trader-workstation-latest.rb
@@ -1,5 +1,5 @@
 cask 'trader-workstation-latest' do
-  version '979,979.4s'
+  version '979.4s'
   sha256 '28debf81e1c4afeb5e918eb7c6c4c13e46aa044c150c4cffccc25ad71e11e37c'
 
   url 'https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-macosx-x64.dmg'
@@ -9,13 +9,13 @@ cask 'trader-workstation-latest' do
   conflicts_with cask: 'trader-workstation'
 
   installer script: {
-                      executable: "#{staged_path}/Trader Workstation #{version.before_comma} Installer.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{staged_path}/Trader Workstation Installer.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 
   uninstall quit:   'com.install4j.5889-6375-8446-2021.22',
             script: {
-                      executable: "#{ENV['HOME']}/Applications/Trader Workstation #{version.before_comma}/Trader Workstation #{version.before_comma} Uninstaller.app/Contents/MacOS/JavaApplicationStub",
+                      executable: "#{ENV['HOME']}/Applications/Trader Workstation/Trader Workstation Uninstaller.app/Contents/MacOS/JavaApplicationStub",
                       args:       ['-q'],
                     }
 end
