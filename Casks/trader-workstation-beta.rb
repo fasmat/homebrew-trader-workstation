2d9c202a816eea6316f3c92223825dd36d14f6f9
Matthias Fasching <matthias.fasching@zuehlke.com>
2021-01-04 15:08:18 +0100

    Add beta and stable versions for Trader Workstation

--- Casks/trader-workstation-beta.rb
+++ Casks/trader-workstation-beta.rb
@@ -1,17 +1,18 @@
 # typed: false
 # frozen_string_literal: true
 
-cask "trader-workstation-latest" do
+cask "trader-workstation-beta" do
   version "981.2h"
   sha256 :no_check
 
-  url "https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-macosx-x64.dmg"
-  name "Trader Workstation Latest"
-  desc "Latest Standalone Trader Workstation"
+  url "https://download2.interactivebrokers.com/installers/tws/beta/tws-beta-macosx-x64.dmg"
+  name "Trader Workstation Beta"
+  desc "Beta Trader Workstation"
   homepage "https://www.interactivebrokers.com/"
 
+  auto_updates true
   conflicts_with cask: "trader-workstation",
-                 cask: "trader-workstation-beta"
+                 cask: "trader-workstation-latest"
 
   installer script: {
     executable: "#{staged_path}/Trader Workstation #{version.major} Installer.app/Contents/MacOS/JavaApplicationStub",
