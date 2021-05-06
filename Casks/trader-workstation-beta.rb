e6fbc63592a45101f1ee11ebc37d2bd199a03f0f
Auto Updater <actions@users.noreply.github.com>
2021-05-06 19:44:07 +0000

    Auto-update TWS Casks: Thu May  6 19:44:07 UTC 2021

--- Casks/trader-workstation-beta.rb
+++ Casks/trader-workstation-beta.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "trader-workstation-beta" do
-  version "984.1a"
+  version "984.1b"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/tws/beta/tws-beta-macosx-x64.dmg"
