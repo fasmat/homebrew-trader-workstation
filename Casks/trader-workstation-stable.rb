6de338036617738edd5abedd96c25845e66b25d8
Auto Updater <actions@users.noreply.github.com>
2021-12-17 06:07:20 +0000

    Auto-update TWS Casks: Fri Dec 17 06:07:20 UTC 2021

--- Casks/trader-workstation-stable.rb
+++ Casks/trader-workstation-stable.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "trader-workstation-stable" do
-  version "981.3f"
+  version "981.3g"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/tws/stable-standalone/tws-stable-standalone-macosx-x64.dmg"
