d498faa8a5207664d86dba6ee46c6c5424fd3ca7
Auto Updater <actions@users.noreply.github.com>
2024-02-29 04:08:21 +0000

    Auto-update TWS Casks: Thu Feb 29 04:08:21 UTC 2024

--- Casks/ibkr-desktop-latest.rb
+++ Casks/ibkr-desktop-latest.rb
@@ -5,7 +5,7 @@ cask "ibkr-desktop-latest" do
   arch arm: "arm", intel: "x64"
   os = on_arch_conditional arm: "macos", intel: "macosx"
 
-  version "0.07.1f"
+  version "0.07.1g"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/latest-standalone/ntws-latest-standalone-#{os}-#{arch}.dmg"
