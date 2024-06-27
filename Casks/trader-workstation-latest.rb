1e882cfab21962e552e85841d6b32d7d15155b1d
Auto Updater <actions@users.noreply.github.com>
2024-06-27 05:08:22 +0000

    Auto-update TWS Casks: Thu Jun 27 05:08:22 UTC 2024

--- Casks/trader-workstation-latest.rb
+++ Casks/trader-workstation-latest.rb
@@ -5,7 +5,7 @@ cask "trader-workstation-latest" do
   arch arm: "arm", intel: "x64"
   os = on_arch_conditional arm: "macos", intel: "macosx"
 
-  version "10.29.1h"
+  version "10.30.1c"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-#{os}-#{arch}.dmg"
