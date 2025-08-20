39e4ba43a2650d6873d0d50660f6f883a71787d7
fasmat <5011972+fasmat@users.noreply.github.com>
2025-08-20 02:41:14 +0000

    Auto-update TWS Casks: Wed Aug 20 02:41:13 UTC 2025

--- Casks/ibkr-desktop-latest.rb
+++ Casks/ibkr-desktop-latest.rb
@@ -5,7 +5,7 @@ cask "ibkr-desktop-latest" do
   arch arm: "arm", intel: "x64"
   os = on_arch_conditional arm: "macos", intel: "macosx"
 
-  version "1.1h"
+  version "1.1j"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/latest-standalone/ntws-latest-standalone-#{os}-#{arch}.dmg"
