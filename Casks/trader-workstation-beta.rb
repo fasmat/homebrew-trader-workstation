3216bf770d83394225871af527f75ba89506c44c
fasmat <5011972+fasmat@users.noreply.github.com>
2026-02-12 03:45:36 +0000

    Auto-update TWS Casks: Thu Feb 12 03:45:36 UTC 2026

--- Casks/trader-workstation-beta.rb
+++ Casks/trader-workstation-beta.rb
@@ -5,7 +5,7 @@ cask "trader-workstation-beta" do
   arch arm: "arm", intel: "x64"
   os = on_arch_conditional arm: "macos", intel: "macosx"
 
-  version "10.44.1c"
+  version "10.44.1d"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/tws/beta/tws-beta-#{os}-#{arch}.dmg"
