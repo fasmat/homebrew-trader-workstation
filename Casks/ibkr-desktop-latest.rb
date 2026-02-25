484019b3f678a03464e77607e278f2f13b2747ac
Matthias Fasching <5011972+fasmat@users.noreply.github.com>
2026-02-25 11:43:34 +0800

    Update Trader Workstation and IBKR Desktop casks (#139)

--- Casks/ibkr-desktop-latest.rb
+++ Casks/ibkr-desktop-latest.rb
@@ -5,7 +5,7 @@ cask "ibkr-desktop-latest" do
   arch arm: "arm", intel: "x64"
   os = on_arch_conditional arm: "macos", intel: "macosx"
 
-  version "2.1f"
+  version "2.1g"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/latest-standalone/ntws-latest-standalone-#{os}-#{arch}.dmg"
