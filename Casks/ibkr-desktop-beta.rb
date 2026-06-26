ba8ede48e44085419f7b4aef883bb54d74cc61b8
fasmat <5011972+fasmat@users.noreply.github.com>
2026-06-26 05:14:11 +0000

    Auto-update TWS Casks: Fri Jun 26 05:14:10 UTC 2026

--- Casks/ibkr-desktop-beta.rb
+++ Casks/ibkr-desktop-beta.rb
@@ -4,7 +4,7 @@
 cask "ibkr-desktop-beta" do
   arch arm: "-arm", intel: "x-x64"
 
-  version "3.3i"
+  version "3.3j"
   sha256 :no_check
 
   url "https://download2.interactivebrokers.com/installers/ntws/beta-standalone/ntws-beta-standalone-macos#{arch}.dmg"
