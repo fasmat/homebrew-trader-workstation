04a470b78fb6d8e4bc9c2fbbeb1be14dc295a979
Matthias <5011972+fasmat@users.noreply.github.com>
2026-09-07 15:21:07 +0200

    Use new DSL

--- Casks/trader-workstation-beta.rb
+++ Casks/trader-workstation-beta.rb
@@ -27,18 +27,17 @@ cask "trader-workstation-beta" do
   }
 
   uninstall_preflight_steps do
-    ohai "Stopping all running instances of Trader Workstation prior to uninstall"
-    begin
-      system_command "/usr/bin/pkill", args: ["-f", "Trader Workstation.app"]
-    rescue RuntimeError
-      ohai "No running instances of Trader Workstation found"
-    end
+    terminate_process "Trader Workstation",
+                     notices:         ["Stopping all running instances of Trader Workstation prior to uninstall"],
+                     failure_message: "Failed to stop instances of Trader Workstation"
 
     # avoids install4j raising a HeadlessException when it tries to move a flagged "protected" file to the Trash
-    ohai "Clearing extended attributes prior to uninstall"
-    ["/Applications/Trader Workstation", "~/Applications/Trader Workstation"].each do |dir|
-      dir = File.expand_path(dir)
-      system_command "/usr/bin/xattr", args: ["-cr", dir], must_succeed: false if File.directory?(dir)
+    if_path_exists "/Applications/Trader Workstation" do
+      run "/usr/bin/xattr", args: ["-cr", "/Applications/Trader Workstation"], must_succeed: false
+    end
+
+    if_path_exists "~/Applications/Trader Workstation" do
+      run "/usr/bin/xattr", args: ["-cr", "~/Applications/Trader Workstation"], must_succeed: false
     end
   end
 
--- Casks/trader-workstation-stable.rb
+++ Casks/trader-workstation-stable.rb
@@ -26,18 +26,17 @@ cask "trader-workstation-stable" do
   }
 
   uninstall_preflight_steps do
-    ohai "Stopping all running instances of Trader Workstation prior to uninstall"
-    begin
-      system_command "/usr/bin/pkill", args: ["-f", "Trader Workstation #{version.major_minor}.app"]
-    rescue RuntimeError
-      ohai "No running instances of Trader Workstation found"
-    end
+    terminate_process "Trader Workstation #{version.major_minor}",
+                     notices:         ["Stopping all running instances of Trader Workstation prior to uninstall"],
+                     failure_message: "Failed to stop instances of Trader Workstation"
 
     # avoids install4j raising a HeadlessException when it tries to move a flagged "protected" file to the Trash
-    ohai "Clearing extended attributes prior to uninstall"
-    ["/Applications/Trader Workstation #{version.major_minor}", "~/Applications/Trader Workstation #{version.major_minor}"].each do |dir|
-      dir = File.expand_path(dir)
-      system_command "/usr/bin/xattr", args: ["-cr", dir], must_succeed: false if File.directory?(dir)
+    if_path_exists "/Applications/Trader Workstation #{version.major_minor}" do
+      run "/usr/bin/xattr", args: ["-cr", "/Applications/Trader Workstation #{version.major_minor}"], must_succeed: false
+    end
+
+    if_path_exists "~/Applications/Trader Workstation #{version.major_minor}" do
+      run "/usr/bin/xattr", args: ["-cr", "~/Applications/Trader Workstation #{version.major_minor}"], must_succeed: false
     end
   end
 
